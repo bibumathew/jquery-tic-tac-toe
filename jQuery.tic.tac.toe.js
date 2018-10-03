1544f2add86b7f5b3bcaa63c3614293ce4e4c51d
Bibu Mathew <bibumathew@gmail.com>
2018-10-03 17:15:47 -0500

    Update jQuery.tic.tac.toe.js

--- jQuery.tic.tac.toe.js
+++ jQuery.tic.tac.toe.js
@@ -5,7 +5,8 @@
 
     $('body').append(
         '<div id="results"></div>' +
-        '<input id="resetBtn" type="button" value="reset" />' +
+        '<input id="resetBtn" type="button" value="reset" /> </br>' +
+        '<div id="player">Single Player<input id="singleBtn" type="checkbox" value="SinglePlayer" /></div>' +
         '<div id="tictac">' +
         '<div id="one"></div>' +
         '<div id="two"></div>' +
@@ -24,14 +25,25 @@
         ticTacToe = $("#tictac"),
         resetBtn = $("#resetBtn"),
         resultDiv = $('#results'),
+        playerDiv = $('#player'),
+        player = $('#singleBtn'),
         ticTacToeChildDiv = ticTacToe.children(),
-        ticTacToeStyle = {
-            "width": "600px",
-            "border": "1px solid black",
-            "overflow": "hidden",
-            "height": "600px",
-            "margin": "0 auto"
-        },
+        singlePlayerMode = false,
+        areEqual = function () {
+            var len = arguments.length;
+            for (var i = 1; i < len; i++) {
+                if (arguments[i] === null || arguments[i] !== arguments[i - 1])
+                    return false;
+            }
+            return true;
+        };
+    ticTacToeStyle = {
+        "width": "600px",
+        "border": "1px solid black",
+        "overflow": "hidden",
+        "height": "600px",
+        "margin": "0 auto"
+    },
         ticTacToeChildStyle = {
             "width": "198px",
             "border": "1px solid green",
@@ -41,21 +53,45 @@
             "text-align": "center"
         },
         resultDivStyle = {
-            "height":"50px",
+            "height": "50px",
             "width": "600px",
             "font-size": "40px"
+        },
+        button = {
+            "background-color": "#4CAF50",
+            "border": "none",
+            "color": "white",
+            "padding": "15px 32px",
+            "text-align": "center",
+            "text-decoration": "none",
+            "display": "inline-block",
+            "font-size": "16px",
+            "margin": "4px 2px",
+            "cursor": "pointer"
         };
     ticTacToe.css(ticTacToeStyle);
     resultDiv.css(resultDivStyle);
     ticTacToeChildDiv.css(ticTacToeChildStyle);
+    playerDiv.css(resultDivStyle);
+    player.css(resultDivStyle);
+    resetBtn.css(button);
+
+    player.on("click", function () {
+        resetBtn.trigger('click');
+        singlePlayerMode = $(this).is(':checked')
+    });
+
     resetBtn.on('click', function () {
         firstPlayer = "X";
         secondPlayer = "0";
         displayValue = secondPlayer;
         ticTacToeChildDiv.html('');
-        resultDiv.html("Start Game");
+        resultDiv.html('')
         ticTacToe.on('click', function (e) {
-            var selectedElement = $(e.target);
+            var selectedElement = $(e.target),
+                non_empty = 0,
+                nonSelected = [];
+
             if (!selectedElement.html()) {
                 displayValue = displayValue == secondPlayer ? firstPlayer : secondPlayer;
                 selectedElement.html(displayValue);
@@ -71,26 +107,39 @@
                 nine = $('#nine').html(),
                 results = "";
 
-            if (one && two && three && four && five && six && seven && eight && nine) {
+            ticTacToeChildDiv.each(function () {
+                var ele = $(this);
+                if (!ele.is(':empty')) {
+                    non_empty++;
+                } else {
+                    nonSelected.push(ele)
+                }
+            });
+            if (non_empty == 9) {
                 resultDiv.html("Game Over !Please reset it");
                 ticTacToe.off('click');
             }
 
-            if (( !!one) && (one == two && two == three) || (one == four && four == seven) ||
-                (one == five && five == nine)) {
+            if (areEqual(one, two, three) || areEqual(one, four, seven) || areEqual(one, five, nine)) {
                 results = one;
-            } else if (( !!three) && (three == six && six == nine) || (three == five && five == seven)) {
+            }
+            else if (areEqual(three, six, nine) || areEqual(three, five, seven)) {
                 results = three;
-            } else if (( !!four) && four == five && five == six) {
+            } else if (areEqual(four, five, six)) {
                 results = four;
-            } else if (( !!seven) && seven == eight && eight == nine) {
+            } else if (areEqual(seven, eight, nine)) {
                 results = seven;
-            } else if (( !!two) && two == five && five == eight) {
+            } else if (areEqual(two, five, eight)) {
                 results = two;
             }
-            if (!!results) {
+            if (results) {
                 resultDiv.html(results + ' Won The Game');
                 ticTacToe.off('click');
+            }
+
+            if (singlePlayerMode && displayValue == firstPlayer && nonSelected.length) {
+                var autoPlayer = nonSelected[Math.floor(Math.random() * nonSelected.length)];
+                autoPlayer.trigger('click')
             }
         });
     });
